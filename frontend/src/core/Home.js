3dbdada14660673f7e5438bdeab80ccb94487001
lakinsha <lakinsha41@gmail.com>
2020-09-10 14:10:34 +0530

    commited by lakinsha

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -1,6 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
+import { getProducts } from './apiCore';
+import Card from './Card';
+import Search from './Search';
 
+const Home = () => {
+  const [productsBySell, setProductsBySell] = useState([]);
+  const [productsByArrival, setProductsByArrival] = useState([]);
+  const [error, setError] = useState(false);
+
+  const loadProductsBySell = () => {
+    getProducts('sold').then(data => {
+      // if (data.error) {
+      //   setError(data.error);
+      // } else {
+      //   setProductsBySell(data);
+      // }
+    });
+  };
+
+  const loadProductsByArrival = () => {
+    getProducts('createdAt').then(data => {
+      console.log(data);
+      // if (data.error) {
+      //   setError(data.error);
+      // } else {
+      //   setProductsByArrival(data);
+      // }
+    });
+  };
 
 
     <Layout
@@ -28,7 +56,11 @@ import Layout from './Layout';
         ))} */}
       </div>
     </Layout>
+	
+  );
+};
 
+export default Home;
 
 
 
