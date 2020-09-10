454d8ba044ebd2604a4141d1a095dc702d1c7b6b
IshiiChapa <67183182+IshiiChapa@users.noreply.github.com>
2020-09-11 02:30:50 +0530

    Committed by Ishani

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -37,12 +37,12 @@ const Home = () => {
 
   return (
     <Layout
-      titel='JewelleryLK'
+      titel='Elegant Esscence'
       description='A brand new online jewellery shopping expirience'
       className='container'
     >
       <Search />
-      <h2 className='mb-4'>New Collection</h2>
+      <h1 className='mb-4'>Elegant Essence</h1>
       <div className='row'>
         {productsByArrival.map((product, i) => (
           <div key={i} className='col-4 mb-3'>
@@ -50,8 +50,11 @@ const Home = () => {
           </div>
         ))}
       </div>
-      <h2 className='mb-4'>Best Selling products</h2>
-      <h2 className='mb-4'>Best Selling products</h2>
+      <h2 className='mb-4'>Every single piece of Elegant Essence's speaks volumes of the rich
+heritage that forms the foundation of this company.</h2>
+      <h3 className='mb-4'>Elegant Essence has proven its reliability through 5 decades of serving you. 
+Elegant Essence has proven its commitment to giving you not only a piece of jewellery but a beautiful experience to 
+cherish throughout your life. Elegant Essence stands by its lifetime’s guarantee by being with you forever.Best Jeweller in Sri Lanka.</h3>
 
       <div className='row'>
         {/* {productsBySell.map((product, i) => (
